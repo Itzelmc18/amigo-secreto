fdb3f0388650099287a775849d46cad6b323f81c
Itzel Mota <imotacast@gmail.com>
2025-02-03 00:40:48 -0600

    Funcion para actualizar lista

--- app.js
+++ app.js
@@ -21,3 +21,14 @@ function agregarAmigo() {
     actualizarLista(); // Actualiza la lista en el HTML
     input.value = ""; // Limpia el input después de agregar el nombre
 }
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos"); // Captura el elemento ul donde se mostrarán los amigos
+    lista.innerHTML = ""; // Limpia la lista antes de actualizarla
+
+    listaAmigos.forEach(nombre => { // Recorre el arreglo listaAmigos
+        let li = document.createElement("li"); // Crea un nuevo elemento li
+        li.textContent = nombre; // Asigna el nombre al contenido del li
+        lista.appendChild(li); // Agrega el li a la lista en el HTML
+    });
+}
