675ce9d779fcdee6087edc73066b8dff4bb93b55
Itzel Mota <imotacast@gmail.com>
2025-02-03 00:32:51 -0600

    Función para asignar texto a un elemento HTML

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 let listaAmigos = [];
 let listaSorteados = [];
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    if (elementoHTML) {
+        elementoHTML.innerHTML = texto;
+    }
+}
